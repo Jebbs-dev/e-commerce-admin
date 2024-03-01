3665675c5ba938c239e34ea3ae175eb6a99fb6d0
Jebbs-dev <ojebiyifulness@gmail.com>
2024-03-01 09:28:26 +0100

    feat: add settings form component

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import * as z from "zod";
+
+import { useState } from "react";
+import { useParams } from "next/navigation";
+import { useEditStore } from "@/mutations/edit-store";
+import { useDeleteStore } from "@/mutations/delete-store";
+
+import { Store } from "@prisma/client";
+
+import toast from "react-hot-toast";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+import AlertModal from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Trash } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+
+
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+export type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams()
+  const origin = useOrigin();
+
+  const { mutateAsync: editStore, isPending } = useEditStore();
+  const { mutateAsync: deleteStore } = useDeleteStore();
+
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (values: SettingsFormValues) => {
+    try {
+      await editStore(values);
+      toast.success("Store updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong!");
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      await deleteStore();
+      toast.success("Store deleted!");
+    } catch (error) {
+      toast.error("Make sure you remove all products and categories first.");
+    } finally {
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={isPending}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button
+          disabled={isPending}
+          variant="destructive"
+          size="icon"
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isPending} className="ml-auto" type="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
+    </>
+  );
+};
+
+export default SettingsForm;
