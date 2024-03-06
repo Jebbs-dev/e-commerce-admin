890abdf4ddef6383455d55e250fb71c7bf5683c8
Jebbs-dev <ojebiyifulness@gmail.com>
2024-03-06 13:15:37 +0100

    feat: add mutation for data table deletion
    Added mutation to delete billboards from the data table

---
+++ mutations/billboard-mutations/delete-billboard-table.ts
@@ -0,0 +1,23 @@
+import { BillboardColumn } from "@/app/(dashboard)/[storeId]/(routes)/billboards/components/columns";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+
+export const useDeleteBillboardTable = () => {
+  const params = useParams();
+  const router = useRouter();
+
+   
+
+  return useMutation({
+    mutationFn: async (dataId: string) => {
+      const response = await axios.delete(
+        `/api/${params.storeId}/billboards/${dataId}`
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      router.refresh();
+    },
+  });
+};
