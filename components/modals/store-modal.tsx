40f86a28374299077e98516b391849a5442171d5
Jebbs-dev <ojebiyifulness@gmail.com>
2024-03-07 10:04:53 +0100

    change file directory

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { useCreateStore } from "@/mutations/add-store";
+import { useCreateStore } from "@/mutations/store-mutations/add-store";
 
 import { Modal } from "@/components/ui/modal";
 
