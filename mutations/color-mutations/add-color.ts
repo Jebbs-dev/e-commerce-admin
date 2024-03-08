bcb748a20ba89597312fb559b9cd10cfab3a2f34
Jebbs-dev <ojebiyifulness@gmail.com>
2024-03-08 14:44:35 +0100

    feat: add mutation to add color

---
+++ mutations/color-mutations/add-color.ts
@@ -0,0 +1,22 @@
+import { ColorFormValues } from "@/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form";
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+
+
+export const useCreateColor = () => {
+
+  const params = useParams();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (values: ColorFormValues) => {
+      const response = await axios.post(`/api/${params.storeId}/colors`, values);
+      return response.data;
+    },
+    onSuccess: () => {
+      router.push(`/${params.storeId}/colors`);
+      router.refresh();
+    },
+  })
+}
