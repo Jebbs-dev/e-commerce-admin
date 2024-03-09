59aeb22002c5f03bd02c8aedc4d512459e13b9fd
Jebbs-dev <ojebiyifulness@gmail.com>
2024-03-09 16:26:34 +0100

    feat: add order client data

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({data}) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Orders (${data.length})`}
+          description="Manage orders for your products"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/orders/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="products" />
+    </>
+  );
+};
